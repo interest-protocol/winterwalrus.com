5797d042b61dc6a1ee6a9650d803a7912a462169
Marco <marcomorete73@hotmail.com>
2025-02-13 12:48:59 +0100

    :construction: wip: work in progress

---
+++ components/stake/stake-form/index.tsx
@@ -0,0 +1,6 @@
+import { Div } from '@stylin.js/elements';
+import { FC } from 'react';
+
+const StakeForm: FC = () => <Div></Div>;
+
+export default StakeForm;
---
+++ components/stake/stake-form/stake-form-field/index.tsx
@@ -0,0 +1,12 @@
+import { Div, Input } from '@stylin.js/elements';
+import { FC } from 'react';
+
+const StakeFormField: FC = () => {
+  return (
+    <Div>
+      <Input />
+    </Div>
+  );
+};
+
+export default StakeFormField;
