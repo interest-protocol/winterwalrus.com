2365e138fa66c95df40bd8646636cea369f34610
Marco <marcomorete73@hotmail.com>
2025-02-27 04:41:08 +0100

    :sparkles: feat: add snow to SWAL

--- components/stake/stake-form/stake-form-field/stake-form-field-coin.tsx
+++ components/stake/stake-form/stake-form-field/stake-form-field-coin.tsx
@@ -1,9 +1,9 @@
-import { Span } from '@stylin.js/elements';
+import { Img, Span } from '@stylin.js/elements';
 import { FC } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 
 import Motion from '@/components/motion';
-import { COIN_ICON, COIN_METADATA } from '@/constants/coins';
+import { COIN_ICON, COIN_METADATA, NFT_IMAGE } from '@/constants/coins';
 
 import { StakeFormFieldCoinProps } from './stake-form-field.types';
 
@@ -13,6 +13,7 @@ const StakeFormFieldCoin: FC<StakeFormFieldCoinProps> = ({ name }) => {
   const coin = useWatch({ control, name: `${name}.coin` }) as string;
 
   const Icon = COIN_ICON[coin];
+  const image = NFT_IMAGE[coin];
 
   return (
     <Motion
@@ -28,9 +29,13 @@ const StakeFormFieldCoin: FC<StakeFormFieldCoinProps> = ({ name }) => {
       justifyContent="center"
       boxShadow="2px 4px 16px 0px #F8F8F80F inset"
     >
-      {Icon && (
+      {Icon ? (
         <Span overflow="hidden" borderRadius="1rem" display="flex">
-          <Icon maxWidth="1.25rem" maxHeight="1.25rem" width="100%" />
+          <Icon maxWidth="2rem" maxHeight="2rem" width="100%" />
+        </Span>
+      ) : (
+        <Span overflow="hidden" borderRadius="0.5rem" display="flex">
+          <Img src={image} width="2rem" height="2rem" />
         </Span>
       )}
       {COIN_METADATA[coin]?.symbol ?? 'Select Coin'}
