f9017b30972574a3349c21e9c5a38f5ff08ebdce
Marco <marcomorete73@hotmail.com>
2025-03-30 02:48:28 +0100

    :sparkles: feat: add validator modal

---
+++ components/settings/settings-menu/settings-menu-validator-modal.tsx
@@ -0,0 +1,105 @@
+import { Div, Input, Label, P, Span } from '@stylin.js/elements';
+import { FC, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { SearchSVG } from '@/components/svg';
+import { useAllowedNodes } from '@/hooks/use-allowed-nodes';
+import { useModal } from '@/hooks/use-modal';
+
+const SettingsMenuValidatorModal: FC = () => {
+  const { handleClose } = useModal();
+  const [search, setSearch] = useState('');
+  const { setValue, getValues } = useFormContext();
+  const { nodes } = useAllowedNodes(getValues('out.type'));
+
+  return (
+    <Div
+      p="1.5rem"
+      gap="1.5rem"
+      height="70vh"
+      width="27rem"
+      display="flex"
+      color="#ffffff"
+      borderRadius="1rem"
+      flexDirection="column"
+      backdropFilter="blur(50px)"
+      bg="linear-gradient(45deg, rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.10))"
+    >
+      <Div display="flex" justifyContent="space-between">
+        <P fontSize="1.25rem" fontWeight="600">
+          Select validator
+        </P>
+        <Span
+          py="0.25rem"
+          px="0.75rem"
+          bg="#FFFFFF1A"
+          display="flex"
+          fontWeight="500"
+          cursor="pointer"
+          borderRadius="0.5rem"
+          onClick={handleClose}
+        >
+          ESC
+        </Span>
+      </Div>
+      <Label
+        px="1rem"
+        gap="0.5rem"
+        bg="#FFFFFF1A"
+        display="flex"
+        alignItems="center"
+        borderRadius="0.75rem"
+      >
+        <Label color="#FFFFFF80">
+          <SearchSVG maxWidth="1.25rem" width="100%" />
+        </Label>
+        <Input
+          py="1rem"
+          width="100%"
+          border="none"
+          outline="none"
+          bg="transparent"
+          color="#FFFFFF80"
+          placeholder="Search asset"
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+      </Label>
+      <Div display="flex" flexDirection="column" gap="1rem" overflow="auto">
+        {nodes
+          ?.filter(
+            ({ id, name }) =>
+              !search ||
+              id.toLowerCase() === search ||
+              name.toLowerCase().includes(search)
+          )
+          .map(({ name, id }) => (
+            <Div
+              p="1rem"
+              key={id}
+              display="flex"
+              cursor="pointer"
+              borderRadius="1rem"
+              border="1px solid #FFFFFF1A"
+              nHover={{ borderColor: '#99EFE44D' }}
+              onClick={() => {
+                setValue('validator', id);
+                handleClose();
+              }}
+            >
+              <Div display="flex" gap="1rem" alignItems="center">
+                <P>{name}</P>
+              </Div>
+              <Div
+                gap="0.25rem"
+                display="flex"
+                textAlign="right"
+                flexDirection="column"
+              ></Div>
+            </Div>
+          ))}
+      </Div>
+    </Div>
+  );
+};
+
+export default SettingsMenuValidatorModal;
--- components/settings/settings-menu/settings-menu-validator.tsx
+++ components/settings/settings-menu/settings-menu-validator.tsx
@@ -1,22 +1,22 @@
 import { Div, P } from '@stylin.js/elements';
-import { AnimatePresence } from 'motion/react';
 import { FC } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { FormProvider, useFormContext } from 'react-hook-form';
 
 import Motion from '@/components/motion';
-import { ChevronRightSVG } from '@/components/svg';
-import { INTEREST_LABS } from '@/constants';
-import { useAllowedNodes } from '@/hooks/use-allowed-nodes';
+import { useModal } from '@/hooks/use-modal';
 
-import { SettingsMenusProps } from './settings-menu.types';
-import SettingsMenuItem from './settings-menu-item';
+import SettingsMenuValidatorModal from './settings-menu-validator-modal';
 
-const SettingsMenuValidator: FC<SettingsMenusProps> = ({
-  show,
-  toggleShow,
-}) => {
-  const { setValue, getValues } = useFormContext();
-  const { nodes } = useAllowedNodes(getValues('out.type'));
+const SettingsMenuValidator: FC = () => {
+  const form = useFormContext();
+  const { setContent } = useModal();
+
+  const openValidator = () =>
+    setContent(
+      <FormProvider {...form}>
+        <SettingsMenuValidatorModal />
+      </FormProvider>
+    );
 
   return (
     <Motion>
@@ -26,46 +26,11 @@ const SettingsMenuValidator: FC<SettingsMenusProps> = ({
         display="flex"
         cursor="pointer"
         alignItems="center"
-        onClick={toggleShow}
+        onClick={openValidator}
         justifyContent="space-between"
       >
         <P>Validator</P>
-        <Motion animate={{ rotate: show ? '90deg' : '0deg' }}>
-          <ChevronRightSVG
-            width="100%"
-            maxWidth="1.25rem"
-            maxHeight="1.25rem"
-          />
-        </Motion>
       </Div>
-      <AnimatePresence>
-        {show && (
-          <Motion
-            ml="1.5rem"
-            overflowY="auto"
-            maxHeight="10rem"
-            style={{ originY: 0 }}
-            exit={{ scaleY: 0, height: 0, opacity: 0 }}
-            animate={{
-              scaleY: [0, 1],
-              height: [0, 'auto'],
-              opacity: [0, 1, 1],
-            }}
-          >
-            {nodes?.map(({ id, name }, index) => (
-              <SettingsMenuItem
-                key={id}
-                name={name}
-                title={name}
-                withBorder={!!index}
-                selected={id === getValues('validator')}
-                onSelect={() => setValue('validator', id)}
-                tag={id === INTEREST_LABS ? 'default' : null}
-              />
-            ))}
-          </Motion>
-        )}
-      </AnimatePresence>
     </Motion>
   );
 };
