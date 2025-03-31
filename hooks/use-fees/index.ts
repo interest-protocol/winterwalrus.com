f5039c44f5dd15cc6cd56aa377e589aad4071367
Marco <marcomorete73@hotmail.com>
2025-03-31 05:20:29 +0100

    :sparkles: feat: add real time fees

--- hooks/use-fees/index.ts
+++ hooks/use-fees/index.ts
@@ -12,12 +12,19 @@ export const useFees = () => {
   const lst = LST_TYPES_MAP[String(query.lst).toUpperCase() || 'WWAL'];
 
   const { data: fees, ...rest } = useSWR([useFees.name, lst], async () => {
+    if (!lst)
+      return {
+        staking: 0,
+        unstaking: 0,
+        transmute: 0,
+      };
+
     const fees = await blizzardSdk.getFees(STAKING_OBJECT[lst]);
 
     return {
-      staking: +fees.mint / 100,
-      unstaking: +fees.burn / 100,
-      transmute: +fees.transmute / 100,
+      staking: Number(fees.mint) / 100,
+      unstaking: Number(fees.burn) / 100,
+      transmute: Number(fees.transmute) / 100,
     };
   });
 
