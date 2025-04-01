4cad59345fd5c6cfaab54dd991585b5b30d96571
Marco <marcomorete73@hotmail.com>
2025-04-01 13:23:40 +0100

    :bug: fix: rpc

--- hooks/use-blizzard-sdk/index.ts
+++ hooks/use-blizzard-sdk/index.ts
@@ -2,14 +2,16 @@ import { BlizzardSDK } from '@interest-protocol/blizzard-sdk';
 import useSWR from 'swr';
 import { useReadLocalStorage } from 'usehooks-ts';
 
-import { RPC, RPC_STORAGE_KEY } from '@/constants';
+import { Network, RPC, RPC_STORAGE_KEY } from '@/constants';
+
+import { RPC_MAP } from './../../constants/rpc';
 
 const useBlizzardSdk = () => {
   const localRPC = useReadLocalStorage<RPC>(RPC_STORAGE_KEY) ?? RPC.Shinami;
 
   const { data } = useSWR<BlizzardSDK>(
     [useBlizzardSdk.name, localRPC],
-    () => new BlizzardSDK({ fullNodeUrl: localRPC })
+    () => new BlizzardSDK({ fullNodeUrl: RPC_MAP[Network.MAINNET][localRPC] })
   );
 
   return data;
