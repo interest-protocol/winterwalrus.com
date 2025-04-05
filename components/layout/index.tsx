3d2dc5416bf10f39f8ebaf1493b56aa0c8d8c427
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2025-04-05 23:17:26 +0100

    :sparkles: feat: expose tvl (#33)

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,10 +1,12 @@
-import { Main } from '@stylin.js/elements';
+import { Div, Footer, Main, Span } from '@stylin.js/elements';
+import Link from 'next/link';
 import { FC, MouseEventHandler, PropsWithChildren } from 'react';
 
 import { useBackgroundMotionTranslate } from '@/hooks/use-background-motion-position';
 import { useBackgroundTranslate } from '@/hooks/use-background-position';
 
 import Header from '../header';
+import { ExternalLinkSVG } from '../svg';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   const { x, y } = useBackgroundMotionTranslate();
@@ -24,13 +26,29 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <Main
+      display="flex"
       minHeight="100vh"
       position="relative"
+      flexDirection="column"
       onMouseLeave={handleMouseLeave}
       onMouseMoveCapture={handleMouseMove}
     >
       <Header />
       {children}
+      <Footer display="flex" justifyContent="center" py="1.5rem">
+        <Link target="_blank" href="https://t.co/1XNRy4vOZ0">
+          <Div
+            gap="0.5rem"
+            display="flex"
+            color="#FFFFFF80"
+            alignItems="center"
+            nHover={{ color: '#99EFE4' }}
+          >
+            <Span>Documentation</Span>
+            <ExternalLinkSVG maxWidth="1rem" width="100%" />
+          </Div>
+        </Link>
+      </Footer>
     </Main>
   );
 };
