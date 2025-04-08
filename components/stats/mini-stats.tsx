7357f5320cd44f13ff978233bfcfcca6dc9bff73
Marco <marcomorete73@hotmail.com>
2025-04-08 14:37:32 +0100

    :zap: chore: change url for metrics

--- components/stats/mini-stats.tsx
+++ components/stats/mini-stats.tsx
@@ -1,5 +1,4 @@
 import { Div, P } from '@stylin.js/elements';
-import { values } from 'ramda';
 import { FC } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
@@ -19,14 +18,7 @@ const MiniStats: FC = () => {
         {(!data || !price) && (isLoading || priceLoading) ? (
           <Skeleton width="4rem" />
         ) : (
-          formatDollars(
-            data
-              ? values(data).reduce(
-                  (acc, { tvl }) => (!isNaN(tvl) ? acc + Number(tvl) : acc),
-                  0
-                ) * (price ?? 1)
-              : 0
-          )
+          formatDollars(data ? Number(data.totalTvl) * (price ?? 1) : 0)
         )}
       </P>
     </Div>
