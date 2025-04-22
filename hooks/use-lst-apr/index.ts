0aaead3acf214719826a714124c68c0ec58dae9c
Marco <marcomorete73@hotmail.com>
2025-04-22 23:42:25 +0100

    :bug: fix: apr

--- hooks/use-lst-apr/index.ts
+++ hooks/use-lst-apr/index.ts
@@ -1,10 +1,32 @@
-import useSwr from 'swr';
+import BigNumber from 'bignumber.js';
+import useSWR from 'swr';
 
-const useLstAPR = (lst: string) =>
-  useSwr([useLstAPR.name, lst], () =>
-    fetch(
-      `https://api.winterwalrus.com/v1/exchange-rate/${lst.split('0x')[1]}`
-    ).then((res) => res.json())
+import { STAKING_OBJECT } from '@/constants';
+import { FixedPointMath } from '@/lib/entities/fixed-point-math';
+
+import useBlizzardSdk from '../use-blizzard-sdk';
+import useEpochData from '../use-epoch-data';
+
+const useLstAPR = (lst: string) => {
+  const blizzardSdk = useBlizzardSdk();
+  const { data: epoch } = useEpochData();
+
+  return useSWR(
+    ['apr', useLstAPR.name, lst, blizzardSdk, !!epoch],
+    async () => {
+      if (!blizzardSdk || !epoch) return { apr: 0 };
+
+      const exchangeRate = await blizzardSdk
+        .toLstAtEpoch({
+          epoch: epoch.currentEpoch,
+          blizzardStaking: STAKING_OBJECT[lst],
+          value: BigInt(FixedPointMath.toBigNumber(1).toString()),
+        })
+        .then((value) => FixedPointMath.toNumber(BigNumber(value ?? 0)));
+
+      return { apr: (1 - exchangeRate) * (365 / 14) };
+    }
   );
+};
 
 export default useLstAPR;
