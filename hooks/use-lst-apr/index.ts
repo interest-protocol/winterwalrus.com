376ea4a9aa3b62a93fe14aa60d4115022f47a00f
Marco <marcomorete73@hotmail.com>
2026-01-06 18:25:48 +0100

    :bug: fix: undefined lst exchange rate

--- hooks/use-lst-apr/index.ts
+++ hooks/use-lst-apr/index.ts
@@ -2,9 +2,11 @@ import useSWR from 'swr';
 
 const useLstAPR = (lst: string) =>
   useSWR([useLstAPR.name, lst], () =>
-    fetch(`https://api.winterwalrus.com/v1/exchange-rate/${lst}`).then((res) =>
-      res.json()
-    )
+    lst
+      ? fetch(`https://api.winterwalrus.com/v1/exchange-rate/${lst}`).then(
+          (res) => res.json()
+        )
+      : Promise.resolve(null)
   );
 
 export default useLstAPR;
