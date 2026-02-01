e63e6b3d97cce0d32163fe83135b85b45959af41
Antonio Kipanda Cardoso <34079870+KipandaJr@users.noreply.github.com>
2026-02-01 22:22:50 +0100

    🔥 feat remove stats page

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,6 +1,5 @@
 export enum RoutesEnum {
   Stake = 'stake',
-  Stats = 'stats',
   DeFi = 'defi',
   Portfolio = 'portfolio',
 }
@@ -8,7 +7,6 @@ export enum RoutesEnum {
 export const Routes: Record<RoutesEnum, string> = {
   [RoutesEnum.Stake]: '/',
   [RoutesEnum.DeFi]: '/defi',
-  [RoutesEnum.Stats]: '/stats',
   [RoutesEnum.Portfolio]: '/portfolio',
 };
 
@@ -16,12 +14,10 @@ export const NAV_ITEMS: ReadonlyArray<RoutesEnum> = [
   RoutesEnum.Stake,
   RoutesEnum.DeFi,
   RoutesEnum.Portfolio,
-  RoutesEnum.Stats,
 ];
 
 export const NAV_ITEMS_TITLE: Record<RoutesEnum, string> = {
   [RoutesEnum.Stake]: 'Stake',
   [RoutesEnum.DeFi]: 'DeFi',
   [RoutesEnum.Portfolio]: 'Portfolio',
-  [RoutesEnum.Stats]: 'Stats',
 };
