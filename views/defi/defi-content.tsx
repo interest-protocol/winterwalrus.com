524f928892c4743f47390007be3e5d4ddaa7593a
KipandaJr <antoniokipandacardoso@gmail.com>
2026-02-19 16:41:15 +0100

    :fire: feat: remove DeFis depreciated

--- views/defi/defi-content.tsx
+++ views/defi/defi-content.tsx
@@ -3,12 +3,7 @@ import { Div } from '@stylin.js/elements';
 import { FC } from 'react';
 import unikey from 'unikey';
 
-import {
-  BluefinSVG,
-  BucketSVG,
-  NoodlesSVG,
-  ScallopSVG,
-} from '@/components/svg';
+import { BluefinSVG, ScallopSVG } from '@/components/svg';
 
 import { DeFiItem } from './components';
 import { DeFiItemProps } from './components/defi-item/defi-item.types';
@@ -51,31 +46,6 @@ const DEFI_ITEMS: ReadonlyArray<DeFiItemProps> = [
           .then((data) => `${Number((+data[0].day.apr.total).toFixed(2))}%`),
       },
     ],
-  },
-  {
-    kind: 'DEX',
-    assets: [TYPES.WWAL],
-    title: 'Trade on Noodles',
-    logo: <NoodlesSVG maxWidth="1.5rem" width="100%" />,
-    link: 'https://noodles.fi/coins/0xb1b0650a8862e30e3f604fd6c5838bc25464b8d3d827fbd58af7cb9685b832bf::wwal::WWAL',
-    metrics: [
-      {
-        name: 'Rank',
-        value: fetch(
-          '/api/third-party/noodles/0xb1b0650a8862e30e3f604fd6c5838bc25464b8d3d827fbd58af7cb9685b832bf::wwal::WWAL'
-        )
-          .then((res) => res.json())
-          .then((data) => `#${Number(+data.data.rank)}`),
-      },
-    ],
-  },
-  {
-    kind: 'CDP',
-    assets: [TYPES.WWAL],
-    title: 'Borrow on Bucket',
-    logo: <BucketSVG maxWidth="1.5rem" width="100%" />,
-    link: 'https://www.bucketprotocol.io/borrow?token=wWAL',
-    metrics: [{ name: 'Interest Rate', value: '14%' }],
   },
 ];
 
